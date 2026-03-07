051d578c73a0a8f090a2367c5fdea2cc2d436173
Joseph <josephelforzli@gmail.com>
2026-03-08 00:59:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from package import *
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+
+app = FastAPI(root_path="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    #allow_origins=["http://localhost:3000","http://10.186.103.211:3000"],  # your Next.js app
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+print('API starting...')
+
+
+
+@app.get("/")
+def test():
+    return {"message": "FastAPI running 🚀"}
+
+class Prompt(BaseModel):
+    text: str
+
+@app.post("/generate")
+def reverse(prompt: Prompt):
+    reversed = prompt.text[::-1]
+    return {"response": reversed}
+
+
---
+++ package/functions.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def functiontest(N=10):
+    return np.arange(N)
